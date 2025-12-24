49e990199bf87887516d6b5c48d2d423213d278e
cihankarimli <kerimlicihan1@gmail.com>
2025-12-25 01:02:04 +0400

    ready for vercel deployment

--- expense-tracker-frontend/next.config.ts
+++ expense-tracker-frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+  // DistDir default olaraq .next olmalıdır
+  distDir: ".next",
+  };
+module.exports = nextConfig;
