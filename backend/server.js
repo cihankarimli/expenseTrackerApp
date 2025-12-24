0d7eb37d2d1723360e08a03169363d022d9536ff
cihankarimli <kerimlicihan1@gmail.com>
2025-12-25 02:24:50 +0400

    `Update CORS origin to include Vercel URL`

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,10 @@ console.log("PORT:", process.env.PORT || "Default: 5000");
 // CORS konfiqurasiyası - daha detallı
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://expense-tracker-app-l9s7-git-main-cihankarimlis-projects.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
